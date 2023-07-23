feeaff8bd51af9d7af6de0d93316a0b8c8397309
Flávio Júnior <flaviojunior9400@gmail.com>
2023-07-23 14:08:08 -0300

    while progressão aritimetica 3.0

---
+++ while_progressao_aritimetica_3.0.py
@@ -0,0 +1,21 @@
+print('==='*13)
+print('        10 TERMOS DE UMA PA')
+print('==='*13)
+
+primeiro = int(input('Digite o primeiro termo: '))
+razao = int(input('RAZÃO: '))
+termo = primeiro
+cont = 1
+total = 0
+mostrar = 10
+
+while mostrar != 0:
+    total = total + mostrar
+    while cont <= total:
+        print(f'{termo} -> ', end='')
+        termo = termo + razao
+        cont = cont + 1
+    print('ACABOU')
+    mostrar = int(input('\nQuantos termos quer mostrar a mais? '))
+    
+print('fim')
